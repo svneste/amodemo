385230d1fab95a180f328ecfecee4633f09e21de
Sergei Nesterovich <sergeinesterovich@MacBook-Pro-Sergei.local>
2024-10-28 08:45:55 +0300

    add kod200

--- src/leads/leads.controller.ts
+++ src/leads/leads.controller.ts
@@ -1,10 +1,20 @@
-import { All, Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  All,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { LeadsService } from './leads.service';
 
 @Controller('leads')
 export class LeadsController {
   constructor(private leadsService: LeadsService) {}
   @All('webhook')
+  @HttpCode(200)
   async getInfoLead(@Body() body) {
     const accountId = body.account.id;
 
@@ -21,11 +31,13 @@ export class LeadsController {
   // сделать запрос по которому мы сможем выгружать сделки отдельно для каждого сотрудника
 
   @Get('user/:id')
+  @HttpCode(200)
   getLeadsForUser(@Param('id') id: string) {
     return this.leadsService.getLeadsForUser(id);
   }
 
   @Get('getall')
+  @HttpCode(200)
   async getLeadsAll() {
     return await this.leadsService.getLeadsAll();
   }
